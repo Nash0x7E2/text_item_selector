4dd3df18341ca1b6df5d192fce940d5eb4d24fea
Nash Ramdial <mail@neevash.dev>
2019-06-22 19:52:56 -0400

    Created test case for index

--- test/text_item_selector_test.dart
+++ test/text_item_selector_test.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:text_item_selector/text_item_selector.dart';
 
+void main() {
+  Widget makeWidgetTestable(Widget child) {
+    return MaterialApp(
+      home: Scaffold(
+        body: child,
+      ),
+    );
+  }
+
+  testWidgets('verify index changes', (WidgetTester tester) async {
+    int activeIndex = 0;
+    await tester.pumpWidget(makeWidgetTestable(
+      ItemSelectorBar(
+        activeIndex: activeIndex,
+        items: ["Item One", "Item Two"],
+        onTap: (int index) => activeIndex = index,
+      ),
+    ));
+    await tester.tap(find.byKey(Key("text-item-1")));
+    await tester.pumpAndSettle();
+    expect(activeIndex, activeIndex = 1);
+    await tester.tap(find.byKey(Key("text-item-0")));
+    await tester.pumpAndSettle();
+    expect(activeIndex, activeIndex = 0);
+  });
+}
